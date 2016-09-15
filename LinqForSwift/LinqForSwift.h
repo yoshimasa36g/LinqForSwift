00c12649785fa73455e2c2530b30ca78ee25282d
yoshimasa36g <ashihana785@gmail.com>
2016-09-15 11:57:20 +0900

    replace project

---
+++ LinqForSwift/LinqForSwift.h
@@ -0,0 +1,19 @@
+//
+//  LinqForSwift.h
+//  LinqForSwift
+//
+//  Created by fssoftware on 2016/09/15.
+//  Copyright © 2016年 yoshimasa36g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinqForSwift.
+FOUNDATION_EXPORT double LinqForSwiftVersionNumber;
+
+//! Project version string for LinqForSwift.
+FOUNDATION_EXPORT const unsigned char LinqForSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinqForSwift/PublicHeader.h>
+
+
--- Podfile
+++
@@ -1,7 +0,0 @@
-platform :ios, "8.2"
-
-source 'https://github.com/CocoaPods/Specs.git'
-use_frameworks!
-
-target 'LinqForSwiftTests' do
-end
